ab1e209d49d95e1f909728fa9519260d0c27ecb8
Narek Ghazaryan <narek.ghazaryan@avetiq.de>
2023-09-11 00:24:46 +0400

    Fix Firebase

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
 import ChatContainer from "./components/ChatContainer.js";
 
-
 function App() {
   return (
-    <div style={{backgroundColor: "#999999" , maxHeight:"100%" , padding:10}} >
-      <ChatContainer/>
+    <div style={{ backgroundColor: "#999999", maxHeight: "100%", padding: 10 }}>
+      <ChatContainer />
     </div>
   );
 }
--- client/src/components/firebaseConfig/firebaseConfig.js
+++ client/src/components/firebaseConfig/firebaseConfig.js
@@ -1,21 +1,33 @@
 // Import the functions you need from the SDKs you need
-import  {initializeApp} from "firebase/app";
-import { getFirestore } from 'firebase/firestore'
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// import { getAnalytics } from "firebase/analytics";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+// TODO: use process.env
+// const firebaseConfig = {
+//     apiKey:process.env.REACT_APP_apiKey,
+//     authDomain:process.env.REACT_APP_authDomain ,
+//     projectId: process.env.REACT_APP_projectId,
+//     storageBucket: process.env.REACT_APP_storageBucket,
+//     messagingSenderId: process.env.REACT_APP_messagingSenderId,
+//     appId:process.env.REACT_APP_appId
+//   };
 
 const firebaseConfig = {
-    apiKey:process.env.REACT_APP_apiKey,
-    authDomain:process.env.REACT_APP_authDomain ,
-    projectId: process.env.REACT_APP_projectId,
-    storageBucket: process.env.REACT_APP_storageBucket,
-    messagingSenderId: process.env.REACT_APP_messagingSenderId,
-    appId:process.env.REACT_APP_appId
-  };
+  apiKey: "AIzaSyCyHdPbO2dQr4W7Cyldmop0hntW3P-_SRg",
+  authDomain: "brainbuildingapp.firebaseapp.com",
+  projectId: "brainbuildingapp",
+  storageBucket: "brainbuildingapp.appspot.com",
+  messagingSenderId: "713949332165",
+  appId: "1:713949332165:web:10c202504b6f2ac94ae6ac",
+  measurementId: "G-643WMQ2450",
+};
 
 // Initialize Firebase
-const app =initializeApp(firebaseConfig)
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 export default getFirestore(app);

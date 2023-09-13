eeca6bf7485d809a8bab285afc2651364699d63a
Narek Ghazaryan <narek.ghazaryan@avetiq.de>
2023-09-13 11:33:30 +0400

    Convert client app to TypeScript

--- client/src/components/ChatContainer.js
+++ client/src/components/ChatContainer.js
@@ -135,7 +135,7 @@ export default function ChatContainer() {
       ) : (
         <UserLogin setUser={setUser} />
       )}
-      <div className="developed-by">Developed by: Narek & Hayk</div>
+      <div className="developed-by">Developed by Narek & Hayk</div>
     </div>
   );
 }

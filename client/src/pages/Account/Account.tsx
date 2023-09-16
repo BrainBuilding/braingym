093f8f9f46bf660222d97335a158f49bbf250e37
Hayk Balyan <hayk.balyan.game@gmail.com>
2023-09-16 14:10:30 +0400

    Addaccount image

--- client/src/pages/Account/Account.tsx
+++ client/src/pages/Account/Account.tsx
@@ -8,6 +8,10 @@ export const Account = () => {
       <h1 className="text-center text-2xl font-bold pt-12">Account</h1>
       <div>
         <p>Welcome, {user?.displayName}</p>
+
+        {user?.photoURL && (
+          <img style={{width: 100, height: 100}} src={user?.photoURL} />
+        )}
       </div>
     </div>
   );

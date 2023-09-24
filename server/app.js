038a6d0ea87231c17bdc7e9d3a83296ee5de77c9
Narek Ghazaryan <narek.ghazaryan@avetiq.de>
2023-09-24 10:39:37 +0400

    Testing BE routing

--- server/app.js
+++ server/app.js
@@ -1,50 +1,48 @@
-const express = require('express')
-const http  = require('http')
-const Server  = require("socket.io").Server
-const app = express()
-const path  = require('path')
+const express = require("express");
+const http = require("http");
+const Server = require("socket.io").Server;
+const app = express();
+const path = require("path");
 
-const server  = http.createServer(app)
-const io = new Server(server , {
-    cors:{
-        origin:"*"
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+  },
+});
+
+const _dirname = path.dirname("");
+const buildPath = path.join(_dirname, "../client/build");
+
+app.use(express.static(buildPath));
+
+app.get("/api/user-details/:userId", function (req, res) {
+  res.send(JSON.stringify(req.params));
+});
+
+app.get("/*", function (req, res) {
+  res.sendFile(
+    path.join(__dirname, "../client/build/index.html"),
+    function (err) {
+      if (err) {
+        res.status(500).send(err);
+      }
     }
-})
+  );
+});
 
+io.on("connection", (socket) => {
+  console.log("We are connected");
 
-const _dirname = path.dirname("")
-const buildPath = path.join(_dirname  , "../client/build");
+  socket.on("chat", (chat) => {
+    io.emit("chat", chat);
+  });
 
-app.use(express.static(buildPath))
-
-app.get("/*", function(req, res){
-
-    res.sendFile(
-        path.join(__dirname, "../client/build/index.html"),
-        function (err) {
-          if (err) {
-            res.status(500).send(err);
-          }
-        }
-      );
-
-})
-
-
-
-
-io.on("connection" , (socket) => {
-   console.log('We are connected')
-
-   socket.on("chat" , chat => {
-      io.emit('chat' , chat)
-   } )
-
-   socket.on('disconnect' , ()=> {
-    console.log('disconnected')
-   })
-})
+  socket.on("disconnect", () => {
+    console.log("disconnected");
+  });
+});
 
 const PORT = process.env.PORT || 5001;
 
-server.listen(PORT , () => console.log(`Listening to port ${PORT}`))
+server.listen(PORT, () => console.log(`Listening to port ${PORT}`));

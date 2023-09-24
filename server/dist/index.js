8bec7eecdfe9b8a180c07d3037e64dd50521609b
Narek Ghazaryan <narek.ghazaryan@avetiq.de>
2023-09-25 01:30:31 +0400

    Trying solution for redirecting

--- server/dist/index.js
+++ server/dist/index.js
@@ -1,4 +1,15 @@
 "use strict";
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
+};
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -65,7 +76,7 @@ var _dirname = path_1.default.dirname("");
 var buildPath = path_1.default.join(_dirname, "../client/build");
 app.use(express_1.default.static(buildPath));
 app.get("/api/user-details/:userId", function (req, res) {
-    res.send(JSON.stringify(req.params));
+    res.send(JSON.stringify(__assign(__assign({}, req.params), { ts: true })));
 });
 app.post("/api/profiles", function (req, res) {
     return __awaiter(this, void 0, void 0, function () {

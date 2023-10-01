e54b37b2ab91e510aaf9272accad92b19e75340d
Narek Ghazaryan <narek.ghazaryan@avetiq.de>
2023-10-01 21:42:56 +0400

    Fix port issue

--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,8 +1,3 @@
 export const SERVER_PORT = 5001;
 export const BASE_URL = window.location.protocol + "//" + window.location.host;
-export const SOCKET_URL =
-  window.location.protocol +
-  "//" +
-  window.location.hostname +
-  ":" +
-  SERVER_PORT;
+export const SOCKET_URL = BASE_URL;
--- server/src/index.ts
+++ server/src/index.ts
@@ -82,6 +82,6 @@ io.on("connection", (socket) => {
   });
 });
 
-const PORT = process.env.CUSTOM_PORT || 5001;
+const PORT = process.env.PORT || 5001;
 
 server.listen(PORT, () => console.log(`Listening to port ${PORT}`));

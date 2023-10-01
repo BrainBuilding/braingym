520cc6b13e68f8622ff1557468a5fd10b7439ad1
Narek Ghazaryan <narek.ghazaryan@avetiq.de>
2023-10-01 22:35:27 +0400

    ix login issue

--- client/src/components/GoogleButton/GoogleButton.hooks.ts
+++ client/src/components/GoogleButton/GoogleButton.hooks.ts
@@ -1,14 +1,20 @@
-import { signInWithPopup } from "firebase/auth";
+import { OAuthCredential, signInWithPopup } from "firebase/auth";
 
 import { auth, provider } from "configs/firebaseConfig";
 import { useCallback } from "react";
 import { createProfileIfNewUser } from "./GoogleButton.utils";
+import { localStore } from "../../utils";
 
 export const useSignInWithGoogle = () => {
   const googleSignIn = useCallback(async () => {
     return signInWithPopup(auth, provider)
-      .then((user) => {
-        createProfileIfNewUser(user);
+      .then((result) => {
+        const user = result.user as unknown as OAuthCredential;
+        const token = user && user.accessToken;
+
+        localStore.setData("token", token);
+
+        createProfileIfNewUser(result);
       })
       .catch((error) => {
         console.error("error[log]::", error);
--- client/src/constants/index.ts
+++ client/src/constants/index.ts
@@ -1,3 +1,10 @@
 export const SERVER_PORT = 5001;
 export const BASE_URL = window.location.protocol + "//" + window.location.host;
-export const SOCKET_URL = BASE_URL;
+export const SOCKET_URL =
+  window.location.hostname === "localhost"
+    ? window.location.protocol +
+      "//" +
+      window.location.hostname +
+      ":" +
+      SERVER_PORT
+    : BASE_URL;
